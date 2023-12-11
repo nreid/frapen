7da909070208ec8bb5de9e03ec0bd877a94d89bb
Noah Reid <noah.reid@uconn.edu>
2023-12-11 15:52:27 -0500

    pstI.R

---
+++ scripts/find_pstI_sites.R
@@ -0,0 +1,80 @@
+library(tidyverse)
+library(Biostrings)     # Provides DNAString, DNAStringSet, etc
+library(BSgenome)       # Provides getSeq()
+library(GenomicRanges)  # Provides GRanges, etc
+library(rtracklayer)    # Provides import() and export()
+
+
+# This R script finds pstI cut sites, flanking sites and writes them to bed files
+
+# sbf1 <- "CCTGCAGG"
+# msei <- "TTAA"
+# pstI <- "CTGCAG"
+
+# read in reference genome 
+seqs <- readDNAStringSet("pennsylvanica.fasta")
+
+# find pstI sites
+mp <- vmatchPattern("CTGCAG",seqs)
+
+# make it a GRanges object
+mp2 <- as(mp, "GRanges")
+
+# make it a data frame
+mp3 <- as.data.frame(mp2)
+mp3[,1] <- str_replace(mp3[,1],regex(" .*"),"")
+
+# 0-index start
+mp3[,2] <- mp3[,2] - 1
+
+# left flank
+left <- mp3
+	left[,2] <- left[,2] - 9
+	left[,3] <- left[,3] - 9
+left[left[,2] < 0,2] <- 0
+
+# right flank
+right <- mp3
+	right[,2] <- right[,2] + 9
+	right[,3] <- right[,3] + 9
+
+# find pstI 1-off sites
+mp_1 <- vmatchPattern("CCTGCAGG",seqs,max.mismatch=1)
+
+# make it a GRanges object
+mp2_1 <- as(mp_1, "GRanges")
+
+# make it a data frame
+mp3_1 <- as.data.frame(mp2_1)
+mp3_1[,1] <- str_replace(mp3_1[,1],regex(" .*"),"")
+
+# 0-index start
+mp3_1[,2] <- mp3_1[,2] - 1
+
+
+# write tables
+write.table(mp3[,1:3],"../meta/pstI.bed",sep="\t",row.names=FALSE,col.names=FALSE,quote=FALSE)
+write.table(mp3_1[,1:3],"../meta/pstI_off.bed",sep="\t",row.names=FALSE,col.names=FALSE,quote=FALSE)
+
+
+# find the distances from pstI to mseI cut sites
+
+# mseI: TTAA
+
+# find msei sites and get granges object
+msei <- vmatchPattern("TTAA",seqs)
+msei2 <- as(msei, "GRanges")
+
+# get closest upstream and downstream msei cut sites to pstI cut sites
+rightind <- precede(mp2,msei2,"first")
+	NOrightNA <- !is.na(rightind)
+leftind <- follow(mp2,msei2,"last")
+	NOleftNA <- !is.na(leftind)
+
+out <- matrix(nrow=length(mp2),ncol=2)
+
+out[NOrightNA,1] <- distance(mp2[NOrightNA,],msei2[rightind[NOrightNA],])
+out[NOleftNA,2] <- distance(mp2[NOleftNA,],msei2[leftind[NOleftNA],])
+
+# `out` now contains all SbfI-MseI RAD fragment lengths in the reference genome. 
+	# column 1 is the left side of SbfI, column 2 is the right. 
